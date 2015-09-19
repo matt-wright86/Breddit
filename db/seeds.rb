364d4f915b26b66e973af6a9a001b2c0b81e107a
justin richards <richards.justind@gmail.com>
2015-09-19 17:42:37 -0500

    small edits to view and added faker data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 first = Post.new     name: "http://www.google.com",
                      description: "a search engine",
                      photo: "http://robohash.org/84Y.png?set=set3&size=150x150"
+
 first.save
 
 second = Post.new    name: "https://tiy-classroom.herokuapp.com/cohorts/7",
@@ -19,7 +20,16 @@ third = Post.new     name: "https://medium.com/@mwright86",
                      description: "A link to Matt's Blog",
                      photo: "http://robohash.org/0ZE.png?set=set3&size=150x150"
 third.save
+
 fourth = Post.new    name: "http://youtube.com",
                      description: "it's this place where people go to watch videos.",
                      photo: "http://robohash.org/K31.png?set=set3&size=150x150"
 fourth.save
+
+20.times do
+  article = Post.new
+  article.name = Faker::Internet.url
+  article.description = Faker::Lorem.paragraphs(1).join("\n\n")
+  article.photo = Faker::Avatar.image
+  article.save
+end
